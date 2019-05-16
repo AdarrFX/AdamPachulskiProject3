c9afe023104f93925173a6b04ccf17d38f0e55e2
AdarrFX <grenador@gmail.com>
2019-05-16 17:10:37 -0400

    Testing animations

--- reader.js
+++ reader.js
@@ -5,6 +5,13 @@ let messageInput = "";
 const blankRowLength = 12;
 let areHolesPunchable = false;
 
+const controlCharacters = [
+    {
+        value: 0,
+        string: ""
+    }
+];
+
 //Clicking the process button triggers the processMessage function which converts the message to binary and punches out the card
 $(".submit-button").on("click", function(event){
 
@@ -67,15 +74,21 @@ const processMessage = function(message){
     
     for(let i = 0; i < messageBinaryArray.length; i++){
         if (messageBinaryArray[i].length < 8){
-            switch (messageBinaryArray[i].length){
-                case 6:
-                    messageBinaryArray[i].unshift("0");
-                    messageBinaryArray[i].unshift("0");
-                    break;
-                case 7:
-                    messageBinaryArray[i].unshift("0");
-                    break;
+            let binaryLength = messageBinaryArray[i].length;
+
+            for(let j = 0; j < (8 - binaryLength); j++){
+                messageBinaryArray[i].unshift("0");
             }
+
+            // switch (messageBinaryArray[i].length){
+            //     case 6:
+            //         messageBinaryArray[i].unshift("0");
+            //         messageBinaryArray[i].unshift("0");
+            //         break;
+            //     case 7:
+            //         messageBinaryArray[i].unshift("0");
+            //         break;
+            // }
         }
     }
 
@@ -104,7 +117,7 @@ const processMessage = function(message){
 
 }//closing for process message function
 
-
+//This function fills the card completely with blank spaces
 function setCardToBlank(){
 
     //clear the punch card of html elements
@@ -113,7 +126,7 @@ function setCardToBlank(){
 
     for(let i = 0; i < blankRowLength; i++){
 
-        //add rows to card
+        //add div rows to card which will hold the holes
         $(".punch-card").append(`<div class="punch-row punch-row${i}"></div>`);
         for(j=0;j < 8; j++){
 
@@ -123,36 +136,62 @@ function setCardToBlank(){
     }
 }//closing for setCardToBlank function
 
+//This section reads the data from holes that have been punched by the user
 function readPunchedHoles() {
+
+    //declare arrays used to hold the binary and decimal values to be converted back to a message
     let binaryArray = [];
     let decimalArray = [];
+
+    //nested for loop which picks a row and then a subsequent hole and reads whether it is punched or not
     for (let i = 0; i < blankRowLength; i++){
+
+        //initialize muldimentional array out of binaryarray
         binaryArray[i] = [];
+
+        //pick a row, and then inside that row, pick a subsequent hole to see if it is punched or not
         for(let j = 0; j < 8; j++){
+
+            //jquery selector built from a string (ex: ".punch-row1 > .punch-hole4") and checks if that exact hole is punched or not. If it is, it pushes a 1 to the proper multidimensional array space, if not, it pushes a 0.
             if ($(".punch-row"+i +" > " + ".punch-hole"+j).hasClass("punch-hole-closed") == true) {
                 binaryArray[i].push("0");
             } else {
                 binaryArray[i].push("1");
             }
         }
+
+        //at the end of every inner loop (once it finishes reading all the holes in a single row) it then collapses that nested array of [1,0,0,1] etc into a single string [1001] as an example.
         binaryArray[i] = binaryArray[i].join("");
     }
     console.log(binaryArray);
 
+    //Parses the array of binary strings to an integer value using base 2 (binary counting) using the parseInt function and pushes it into the decimal array. Each punch card row now has a decimal number representation of its binary character.
     binaryArray.forEach(binaryValue => {
         decimalArray.push(parseInt(binaryValue, 2));
     });
 
+    //init the string variable which will return the translated message
     let translatedString = "";
 
+    //converts the decimal value of the decimal array to a string
     decimalArray.forEach(decimalValue => {
         translatedString += String.fromCharCode(decimalValue);
     });
 
     console.log(decimalArray);
 
+    //return the final translated message
     return translatedString;
 
 }//closing for readPunchedHoles function
+
+const animateCard = {
+    advanceUpOneRow: function() {
+        $(".punch-card").addClass("oneRowUp");
+        // $(".punch-card").css("top", "-35px");
+        // $(".punch-card").removeClass("oneRowUp");
+
+    }
+};
 
 }); //closing for document ready function
